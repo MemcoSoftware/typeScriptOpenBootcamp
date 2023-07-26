6440744e9929cc6ee521cd3e4e81e2ffc4cb124a
Miguel Herrera <software@memcosas.com>
2023-07-26 17:27:04 -0400

    generating scripts to automate the tsc

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hi Baned!");
+console.log("adios Baned!");
+//this is a comment
+/**
+ * more comments
+ * multiline
+ */ 
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+console.log("Hi Baned!");
+console.log("adios Baned!");    
+
+//this is a comment
+
+/**
+ * more comments
+ * multiline
+ */
+
+//declaracion variales 
+
+var nombre: string = "Baned";
+
+let email = "software@memcosas.com"; //variable local
+
+const PI: number = 3.1416;
+
+console.log(`Hola ${nombre}`);
+
+console.log(`Email de ${nombre} es ${email}`);
+
